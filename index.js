a2b639d0e24e1b85a519ffd3da1510225c003ae3
yaroslav <sadoctober2017@gmail.com>
2019-05-24 11:16:11 +0300

    i am andr

--- index.js
+++ index.js
@@ -26,5 +26,6 @@ bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
     // send a message to the chat acknowledging receipt of their message
-    bot.sendMessage(chatId, 'Received your message');
+    if (msg === 'Я не Андрей')
+        bot.sendMessage(chatId, 'Я Андроид');
 });
