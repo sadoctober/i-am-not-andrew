28ace6f669b8f25487fc1191313fcefb7d42f30a
yaroslav <sadoctober2017@gmail.com>
2019-05-24 11:33:50 +0300

    fee

--- index.js
+++ index.js
@@ -26,6 +26,5 @@ bot.on('message', (msg) => {
     const chatId = msg.chat.id;
 
     // send a message to the chat acknowledging receipt of their message
-    if (msg === 'Я не Андрей')
-        bot.sendMessage(chatId, 'Я Андроид');
+    bot.sendMessage(chatId, 'Я Андроид');
 });
